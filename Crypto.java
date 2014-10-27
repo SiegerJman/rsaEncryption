8483e179606fb63e5c628272d1885c3593acdfbe
SiegerJman <reedjs3@miamioh.edu>
2014-10-27 15:43:08 -0400

    First commit of RSA HW

---
+++ Crypto.java
@@ -0,0 +1,23 @@
+//Justin Reed
+//10/27/14
+//CSE 270M
+public class Crypto {
+	
+	public static void main(String [ ] args)
+	{
+		int p = 0;
+		int q = 0;
+		int n = 0;
+		
+		System.out.println("Part 1 - RSA KEY GENERATION");
+		System.out.println("Enter 2 prime numbers, and I will help you generate public and private");
+		System.out.println("RSA keys.");
+		System.out.println("Enter prime p: ");
+		//GET P HERE
+		System.out.println("Enter prime q: ");
+		//GET Q HERE
+		System.out.println("Thank you. Keep these primes secret.");
+		System.out.println("n = pq = " +p*q + " (share this value)");
+		//SET N HERE
+	}
+}
